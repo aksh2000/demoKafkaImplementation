5314efe037cba1ba3cd1a14054071c6aaa8c917f
akshithvarma <akshithchittiveli@gmail.com>
2021-02-09 14:05:21 +0530

    first commit

---
+++ apache-kafka-consumer/src/test/java/com/aksh/apachekafkaconsumer/ApacheKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksh.apachekafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
